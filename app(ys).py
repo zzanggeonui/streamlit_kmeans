d55e9df662e0be0ebef3fdbc8bc18acf2813db7c
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2022-12-16 12:54:13 +0900

    final

---
+++ app(ys).py
@@ -0,0 +1,78 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+import streamlit as st
+import pandas as pd 
+
+def main():
+    st.title('K-Means 클러스터링')
+
+## 1.유저가 클러스터링 하고싶은 csv파일을 업로드
+
+    file = st.file_uploader('클러스터링시켜드림', type=['csv'])
+
+##2.업로드한 csv파일을 데이터프레임으로 읽고
+    if file is not None :
+        df = pd.read_csv(file)
+        st.dataframe( df )
+
+        ##4.wcss를 확인하기 위한, 그룹의 갯수를 정할수있다.(1~10개)
+        columns = df.columns
+        selected_columns = st.multiselect('원하는 컬럼을 선택하세요' , columns )
+
+        if len(selected_columns) != 0 :
+
+            X = df[selected_columns]
+            st.dataframe(X)
+
+            st.subheader('wcss를 위한 클러스터링 갯수를 선택')
+
+            max_number = st.slider('최대 그룹 선택',2,10,value=5)
+
+
+            wcss = []
+            for k in np.arange(1, max_number+1) :
+                kmeans = KMeans(n_clusters= k, random_state=5)
+                kmeans.fit(X)
+                wcss.append( kmeans.inertia_ )
+
+            ##5.엘보우 메소드 차트를 화면에 표시
+            fig1 = plt.figure()
+            x = np.arange(1, max_number +1)
+            plt.plot( x, wcss )
+            plt.title('The Elbow Method')
+            plt.xlabel('Number of Clusters')
+            plt.ylabel('WCSS')
+            st.pyplot(fig1)     
+
+
+            ## 6. 실제 그룹핑하고 싶은 갯수를 입력
+            #k = st.slider('그룹 갯수 결정',1,max_number)
+
+            k =st.number_input('그룹 갯수 결정',1,max_number)
+            kmeans = KMeans(n_clusters= k, random_state=5)
+
+            y_pred = kmeans.fit_predict(X)
+
+            df['Group'] = y_pred
+
+            ##7.위에서 입력한 그룹의 갯수로 클러스터링하여 결과를 보여준다
+            st.dataframe(df.sort_values('Group'))
+
+            df.to_csv('result.csv')
+
+## 1.비어있는 데이터 처리가 안되어있다
+#2.숫자로 되어있는 컬럼만 했는데 문자로 되어있는 컬럼 처리
+  #카테고리컬 데이터가 2개냐 3개이상이냐(3개이상이면 원핫)
+#3. 유저 인터랙티브한 데이터 분석 기능 추가
+
+if __name__ == '__main__' :
+    main()
+
+
+
+
+
+
+
+
